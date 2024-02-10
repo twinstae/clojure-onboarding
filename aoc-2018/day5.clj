f629b99937923c85cd71c0e818d5df51975e0f78
Taehee Kim <rabolution@gmail.com>
2024-02-10 23:13:23 +0900

    feat: day5 part 1

---
+++ aoc-2018/day5.clj
@@ -0,0 +1,49 @@
+(ns day5
+  (:require [clojure.test :refer [deftest is testing]]))
+
+(defn react?
+  [a b]
+  (= (abs (- (int a) (int b))) 32))
+
+(deftest react?-test
+  (testing "반응한다"
+    (is (react? \a \A))
+    (is (react? \A \a))
+    (is (react? \b \B)))
+  (testing "반응하지 않는다"
+    (is (false? (react? \a \B)))
+    (is (false? (react? \A \A)))
+    (is (false? (react? \a \a)))))
+
+(defn reaction
+  [polymer]
+  (->> polymer
+       (reduce
+        (fn [acc unit]
+          (if (and (seq acc) (react? unit (first acc)))
+            (rest acc)
+            (cons unit acc)))
+        '())
+       (reverse)
+       (apply str)))
+
+(deftest reaction-test
+  (testing "반응해서 사라진다"
+    (is (= (reaction "aA") ""))
+    (is (= (reaction "Aa") "")))
+  (testing "연쇄 반응으로 사라진다"
+    (is (= (reaction "abBA") ""))
+    (is (= (reaction "aBbA") "")))
+  (testing "같은 종류가 아니면 반응이 일어나지 않는다"
+    (is (= (reaction "abAB") "abAB"))
+    (is (= (reaction "aBAb") "aBAb")))
+  (testing "같은 종류여도 극성이 같으면 반응이 일어나지 않는다"
+    (is (= (reaction "aabAAB") "aabAAB"))
+    (is (= (reaction "bbAbaBB") "bbAbaBB")))
+  (testing "복잡한 예시"
+    (is (= (reaction "dabAcCaCBAcCcaDA") "dabCBAcaDA"))))
+
+(def input (slurp "resources/day5/input1.txt"))
+
+(comment
+  (count (reaction input)))
