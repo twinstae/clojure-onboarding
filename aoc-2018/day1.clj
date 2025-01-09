5dd5f2dbd8b24eadf7705b5a9571512076f78068
김태희 <taehee.kim@sherpas.team>
2025-01-09 17:12:17 +0900

    feat: lazy sequence 처리

--- aoc-2018/day1.clj
+++ aoc-2018/day1.clj
@@ -1,25 +1,31 @@
 (ns day1
   (:require
-   [clojure.string :as str]))
-
-(def data-file (slurp "resources/day1/input1.txt"))
-(def input-lines (str/split-lines data-file))
-
-(defn sum [s] (reduce + s))
+   [clojure.java.io :as io]))
 
 
-(def input (map parse-long input-lines))
 (comment
-  ;; part 1
+  ;; part 1 
+  (def input (with-open [rdr (io/reader "resources/day1/input1.txt")]
+               (map parse-long (line-seq rdr))))
 
-  (sum input))
+  (prn (reduce + input)) ; Execution error (IOException) at java.io.BufferedReader/ensureOpen (BufferedReader.java:121).
+  ; Stream closed
+
+
+  (with-open [rdr (io/reader "resources/day1/input1.txt")]
+    (let [input (map parse-long (line-seq rdr))]
+      (reduce + input)))
+  )
 
   ;; part2
-(->> (cycle input)
-     (reduce
-      (fn [[seen current] change]
-        (let [next (+ current change)]
-          (if (contains? seen next)
-            (reduced next)
-            [(conj seen next) next])))
-      [#{0} 0]))
+(with-open [rdr (io/reader "resources/day1/input1.txt")]
+  (let [input (map parse-long (line-seq rdr))]
+   (->> (cycle input)
+        (reduce
+         (fn [[seen current] change]
+           (let [next (+ current change)]
+             (if (contains? seen next)
+               (reduced next)
+               [(conj seen next) next])))
+         [#{0} 0]))))
+
